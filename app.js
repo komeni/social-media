d76958d5481de467253cc4a7afada30dceb2402e
komeni <hayatullahcr7@gmail.com>
2022-05-06 21:32:43 +0100

    feat: add responsiveness

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // alert(2);
+const menuItems = document.querySelectorAll('.menu-item')
+const messagesNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notifications'){
+            document.querySelector('.notifications-popup').style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
+
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none'
+    setTimeout(() => {
+        messages.style.boxShadow = 'none'
+    }, 2000)
+})
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    console.log(val);
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
+messageSearch.addEventListener('keyup', searchMessage);
